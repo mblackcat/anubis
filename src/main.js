41b4d8af4a0aba4b9f457a5a5b691ba120a6af52
Joey <275980464@qq.com>
2019-03-21 00:50:28 +0800

    uikit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
 Vue.config.productionTip = false
 
