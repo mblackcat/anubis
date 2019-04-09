bde764e6d0b51844a272e3859ae3ed3730073efa
275980464@qq.com <youyou2357>
2019-04-09 11:41:25 +0800

    try import css

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import 'uikit/dist/css/uikit.min.css'
 
 Vue.config.productionTip = false
 
